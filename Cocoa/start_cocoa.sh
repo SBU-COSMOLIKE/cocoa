81aa986f5b8450b563cea4162c13b6767d0accd9
Vivian Miranda <vivian.miranda.astro@gmail.com>
2026-04-03 19:59:11 -0400

    merged main

--- Cocoa/start_cocoa.sh
+++ Cocoa/start_cocoa.sh
@@ -226,19 +226,11 @@ if [[ -z "${IGNORE_EMULTRF_CODE}" ]]; then
   COBTH="${ROOTDIR:?}/cobaya/cobaya/theories"
   TMP="${EMULTRF_NAME:-"emultrf"}"
 
-  for TMP2 in \
-    "emulcmb" \
-    "emulbaosn" \
-    "emultheta" \
-    "emulrdrag" \
-    "emul_cosmic_shear" \
-    "emul_ggl" \
-    "emul_wtheta" \
-    "emulmps"
-  do
+  for TMP2 in "emulcmb" "emulbaosn" "emultheta" "emulrdrag" \
+              "emul_cosmic_shear" "emul_ggl" "emul_wtheta"; do
     if [[ ! -L "${COBTH:?}/${TMP2}" ]]; then
       ln -s "${ECODEF:?}/emulators/${TMP}/${TMP2}" "${COBTH:?}" \
-        >>"${OUT1:?}" 2>>"${OUT2:?}" || { error_start_cocoa "${EC34:?}"; return 1; }
+        >>${OUT1:?} 2>>${OUT2:?} || { error_start_cocoa "${EC34:?}"; return 1; }
     fi
   done
 
--- Cocoa/stop_cocoa.sh
+++ Cocoa/stop_cocoa.sh
@@ -159,18 +159,10 @@ fi
 if [[ -z "${IGNORE_EMULTRF_CODE}" ]]; then
   COBTH="${ROOTDIR:?}/cobaya/cobaya/theories"
 
-  for TMP in \
-    "emulcmb" \
-    "emulbaosn" \
-    "emultheta" \
-    "emulrdrag" \
-    "emul_cosmic_shear" \
-    "emul_ggl" \
-    "emul_wtheta" \
-    "emulmps"
-  do
-    if [[ -L "${COBTH:?}/${TMP:?}" ]]; then
-      rm -f "${COBTH:?}/${TMP:?}"
+  for TMP in "emulcmb" "emulbaosn" "emultheta" "emulrdrag" \
+             "emul_cosmic_shear" "emul_ggl" "emul_wtheta"; do
+    if [[ -L "${COBTH:?}/${TMP}" ]]; then
+      rm -f "${COBTH:?}/${TMP}"
     fi
   done
 
@@ -180,6 +172,7 @@ fi
 # ----------------------------------------------------------------------------
 # ------------------------ STOP EXTERNAL PROJECTS ---------------------------
 # ----------------------------------------------------------------------------
+
 
 if [ -n "${ROOTDIR}" ]; then
   source "${ROOTDIR:?}/installation_scripts/stop_all_projects.sh"
